10838f8ba4ba0752a2d94162e397a416a1641226
moen <moenr49@gmail.com>
2025-06-21 00:41:03 +0300

    final commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31 * 2 = 62
 
-// Write your code here
+// 2. random - should be a random integer > 0
+let random = Math.floor(Math.random() * 100) + 1; // generates a random integer between 1 and 100
+
+// 3. mod - remainder of 24 % 5 = 4
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; // 24 % 5 = 4
+
+// 4. max - should return 20 as highest number
+let max = Math.max(1, 3, 4, 20); // returns 20
